be437c325b1cb43c690553ab5d68d1a937bc6661
Allan Wu <allan.wu@outlook.com.au>
2026-03-03 11:39:43 +0800

    Added a subtract function

--- calculator.py
+++ calculator.py
@@ -1,5 +1,10 @@
+print("Hello, my name is Allan.")
+
 def add_two_numbers(x, y):
     return x+y
+
+def subtract_two_numbers(x, y):
+    return x-y
 
 if __name__ == "__main__":
     print(add_two_numbers(1,2))
